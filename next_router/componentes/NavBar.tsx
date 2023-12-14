1a75fc07833b6545efa2b892df7c3903799e9a57
MINT <pb1123love@gmail.com>
2023-12-14 22:02:49 +0900

    styled jsx

--- next_router/componentes/NavBar.tsx
+++ next_router/componentes/NavBar.tsx
@@ -1,18 +1,28 @@
 import Link from "next/link";
 import {useRouter} from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
     const router = useRouter();
     return (
-        <nav className={styles.nav}>
-            <Link href={"/"} style={{color: router.pathname === "/" ? "red" : "blue"}}>
-                Home
+        <nav>
+            <Link href={"/"} legacyBehavior>
+                <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
             </Link>
             |
-            <Link href={"/about"} style={{color: router.pathname === "/about" ? "red" : "blue"}}>
-                About
+            <Link href={"/about"} legacyBehavior>
+                <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
             </Link>
+            <style jsx>
+                {`
+                    a {
+                        text-decoration: none;
+                    }
+
+                    .active {
+                        color: tomato;
+                    }
+                `}
+            </style>
         </nav>
     )
 }
