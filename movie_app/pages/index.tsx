f12412bfd1c33670898ca42e0c6c1c719c252431
MINT <pb1123love@gmail.com>
2023-12-19 16:16:09 +0900

    fetch data

--- movie_app/pages/index.tsx
+++ movie_app/pages/index.tsx
@@ -1,10 +1,37 @@
 import Seo from "@/components/Seo";
+import {useEffect, useState} from "react";
+
+
+const API_KEY = "";
+
+interface IfMovie {
+    id: number;
+    original_title: string;
+}
 
 export default function Home() {
+    const [movies, setMovies] = useState<IfMovie[]>();
+
+    useEffect(() => {
+        (async () => {
+            const {results} = await (
+                await fetch(
+                    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+                )
+            ).json();
+            setMovies(results);
+        })();
+    }, []);
+
     return (
-        <div>
+        <div className="container">
             <Seo title="Home"/>
-            <h1>Home</h1>
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map((movie: IfMovie) => (
+                <div key={movie.id}>
+                    <h4>{movie.original_title}</h4>
+                </div>
+            ))}
         </div>
     )
 }
